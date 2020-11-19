8b7159abfca7d3093da9f9887a0bac545214edcd
Jenna Horrall <jennahorrall@Jennas-MacBook-Air.local>
2020-11-19 13:53:04 -0500

    made post component, fetched data from db

---
+++ src/Post.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+const host = "https://proj2-api.callanhand.me:8442/post";
+
+export function Post() {
+
+    return (
+    <div className="post">
+    </div>
+    );
+
+}
+
+
+
