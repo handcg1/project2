82df5bf6b668895f5dbca6a9ad25660736031db4
Jenna Horrall <jennahorrall@Jennas-MacBook-Air.local>
2020-12-02 16:15:14 -0500

    working on modal with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import './App.css';
 import React, { useRef, useEffect } from 'react';
 import film from './Film-1.png';
-import {Post} from './Post';
+import { Post } from './Post';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchPosts, uploadPost } from './actions';
-//const host = "https://proj2-api.callanhand.me:8442";
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
 
 function newPost() {
   return (<div className="new-post">modal</div>);
 }
 
+
 function App() {
 
+  const [show, setShow] = useState(true);
+  const [username, setUsername] = useState("");
 
   const posts = useSelector(state => state.posts);
   const dispatch = useDispatch();
-
-  //const initial_images = [{username: 'jennah', caption: 'hey', image: Image}];
-  // OR initial_images = [{username: 'jennah', caption: 'hey', image_url: host/images/image_name}];
-
 
   useEffect(() => {
     dispatch(fetchPosts());
@@ -29,32 +30,57 @@ function App() {
   const onUpload = event => {
     const fileInput = fileInputRef.current;
     if (fileInput.files.length > 0) {
-      setTimeout(() => dispatch(uploadPost(fileInput.files[0], document.getElementById("username").value, document.getElementById("caption").value)), 60000);
-      
+      setTimeout(() => dispatch(uploadPost(fileInput.files[0], username, document.getElementById("caption").value)), 60000);
     }
   }
 
+  const handleClose = () => {
+    document.getElementById("top-class").style.opacity = 1;
+    var errorBox = document.getElementById("error");
+    if (username.length < 3) {
+      errorBox.innerHTML = "Please enter a username longer than 2 characters.";
+      return;
+    }
+    setShow(false);
+  }
+
+
   return (
 
-    <div className="App">
+
+
+    <div className="App" id="top-class">
+
+      <Modal className="username-modal" 
+        animation={false}
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}>
+
+        <Modal.Header className="username-modal-header">
+          <Modal.Title>Welcome to Memory Stack!</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="username-modal-body">
+          <p></p>
+          <input className="username" type="text" onChange={e => setUsername(e.target.value)}></input>
+        </Modal.Body>
+        <Modal.Footer className="username-modal-footer">
+        <div id="error"></div> 
+          <Button variant="primary" onClick={handleClose}>
+            Enter
+            </Button>
+        </Modal.Footer>
+      </Modal>
+
 
       <div>
-        <label>
-          Username:
-          <input id="username" type="text" name="name" />
-        </label>
         <label>
           Caption:
           <input id="caption" type="text" name="caption"></input>
         </label>
         <input type="file" ref={fileInputRef} />
         <button onClick={onUpload}>upload</button>
-      </div>
-
-      <div>
-        <ul >
-          {posts.map(post => <Post key={post.id} post={post}/>) }
-        </ul>
       </div>
 
       <div className="top-header">
@@ -64,7 +90,15 @@ function App() {
         <a className="link" href="post.html">Scheduled Posts</a>
         <a className="link" href="post.html">Profile</a>
       </div>
+
+      <div>
+        <ul >
+          {posts.map(post => <Post key={post.id} post={post} />)}
+        </ul>
+      </div>
     </div>
+
+    
   );
 }
 
